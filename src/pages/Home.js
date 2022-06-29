bebdf89503578d10f8be953975ed8b3da42f5001
jentebuijs <jentebuijs@gmail.com>
2022-06-29 11:18:30 +0200

    tot bonus1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Link } from 'react-router-dom';
+import {AuthContext} from "../context/AuthContext";
 
 function Home() {
+  const data = useContext(AuthContext);
+  console.log(data);
+
   return (
     <>
       <h1>Homepagina</h1>
